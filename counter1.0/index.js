5e32f3f21784b244404ea986f30dd96ae9c84804
m3ph3dr0n <dawidlaska888@gmail.com>
2021-11-22 19:40:29 +0100

    added counter 1.0 page

---
+++ counter1.0/index.js
@@ -0,0 +1,31 @@
+﻿// document.getElementById("count-el").innerText = 5;
+
+let count = 0, save
+let countEl = document.getElementById("count-el")
+let saveEL = document.getElementById("save-el")
+
+function Increment(){
+
+    count+=1
+    countEl.textContent = count
+}
+
+function Decrement(){
+
+    if(count>0){
+        count-=1
+        countEl.textContent = count
+    }
+}
+
+function Save(){
+
+    saveEL.textContent += count + " - " //textContent reads not only characters
+    countEl.textContent = count = 0;
+}
+
+function Reset(){
+    saveEL.textContent = "Previous entries: "
+}
+
+
